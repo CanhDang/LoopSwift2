a42127ab446dde766eedf663899e73190e9f11e3
HuuLuong <huuluong@techmasters-Mac-mini-2.local>
2016-07-14 15:50:59 +0700

    second commit

--- LoopSwift2/ViewController.swift
+++ LoopSwift2/ViewController.swift
@@ -10,16 +10,91 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tf_number: UITextField!
+    
+    
+    let margin:CGFloat = 50
+    let marginTop:CGFloat = 30
+    
+    var numberOfBall: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tf_number.layer.borderWidth = 1
+        tf_number.layer.borderColor = UIColor.blackColor().CGColor
+        
+        
+    }
+    
+    func drawBalls(){
+        for indexRow in 0..<numberOfBall{
+                for indexColumn in 0..<numberOfBall {
+                    let image = UIImage(named: "green")
+                    let ball = UIImageView(image: image)
+                    ball.center = CGPointMake(margin + CGFloat(indexColumn) * spaceBetweenColumn(), margin + marginTop + CGFloat(indexRow) * spaceBetweenRow())
+                    
+                    self.view.addSubview(ball)
+                }
+           
+        }
+        
+        
+    }
+    
+    func spaceBetweenColumn()-> CGFloat{
+        
+        let space = (self.view.bounds.size.width - 2 * margin)/(CGFloat(numberOfBall-1))
+        
+        return space
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func spaceBetweenRow() -> CGFloat {
+        let space = (self.view.bounds.size.height - marginTop - 2 * margin)/(CGFloat(numberOfBall-1))
+        
+        return space
     }
+    
+    @IBAction func action_draw(sender: AnyObject) {
+        
+        for view in self.view.subviews{
+            if view is UIImageView {
+                view.removeFromSuperview()
+            }
+        }
+        
+        if let tempNumber = Int(tf_number.text!) {
+            
+            if tempNumber > 0 {
+                
+                numberOfBall = tempNumber
+                
+                if numberOfBall == 1{
+                    
+                    drawOneBall()
+                } else{
+                
+                    drawBalls()
+                }
+            } else {
+                print("invalid number")
+            }
+            
+        } else {
+            print("invalid number")
+        }
+        
+    }
+    
+    func drawOneBall() {
+        
+        let image = UIImage(named: "green")
+        let ball = UIImageView(image: image)
+        ball.center = CGPointMake(self.view.bounds.size.width / 2 , (self.view.bounds.size.height-marginTop) / 2 )
+        
+        self.view.addSubview(ball)
 
+    }
 
 }
 
---
+++ README.md
@@ -0,0 +1,5 @@
+LoopSwift2 written by Dang Dinh Canh
+for iOS Swift class 
+
+Input a number >= 1 and click Draw, the green balls will appear
+There are n*n balls
